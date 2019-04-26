d3927cd721a8b11a947752fcca3775242b23600b
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-25 22:45:42 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/placarapp/ClubesActivity.kt
@@ -0,0 +1,27 @@
+package br.com.fiap.placarapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.placarapp.extensions.value
+import kotlinx.android.synthetic.main.activity_clubes.*
+
+class ClubesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clubes)
+
+        BtIniciar.setOnClickListener { minhaView ->
+            irParaOutraTela()
+        }
+
+    }
+
+    fun irParaOutraTela(){
+        val proximaTela = Intent(this,PlacarActivity::class.java)
+        proximaTela.putExtra("TIME_CASA", InputClubeCasa.value())
+        proximaTela.putExtra("TIME_VISITANTE", InputClubeVisitante.value())
+        startActivity(proximaTela)
+    }
+}
